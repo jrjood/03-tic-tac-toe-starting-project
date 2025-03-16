8e5edeff0cd3a80b0c6b610cfbcd11467cd473cb
jrjood <youssifbebo@gmail.com>
2025-03-16 23:54:52 +0200

    Added responsive web design

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/03-tic-tac-toe-starting-project/", // Set repo name here
+  build: {
+    outDir: "dist",
+  },
+});
